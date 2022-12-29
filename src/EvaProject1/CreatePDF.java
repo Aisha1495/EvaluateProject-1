b3a5ea8c16c61ab93a29eb8667485147ec65c48a
Aisha1495 <aishaalmamari6@gmail.com>
2022-12-29 03:09:46 -0800

    create 100 pdf using loop fixed

--- src/EvaProject1/CreatePDF.java
+++ src/EvaProject1/CreatePDF.java
@@ -1,4 +1,5 @@
 package EvaProject1;
+
 import java.io.FileOutputStream;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Paragraph;
@@ -7,37 +8,34 @@ import com.itextpdf.text.pdf.PdfWriter;
 public class CreatePDF {
 
 	public static void main(String[] args) {
-		 try {
-			int i = 0 ;
-			while(i<=100) {
-		      // Create a new document
-		      Document document = new Document();
+		try {
+//			String dir = "/path/to/pdf/directory/";
+//			String searchWord ="Hello";
+			
+			int i = 0;
+			while (i <= 100) {
+				// Create a new document
+				Document document = new Document();
 
-		      // Create a PDF writer
-		      PdfWriter.getInstance(document, new FileOutputStream("document.pdf" + i + ".pdf"));
-		      
-		    
-			 
+				// Create a PDF writer
+				PdfWriter.getInstance(document, new FileOutputStream("document.pdf" + i + ".pdf"));
 
-		      // Open the document
-		      document.open();
+				// Open the document
+				document.open();
 
-		      // Add some content
-		      document.add(new Paragraph("Hello, World!"));
-			 
-		      // Close the document
-		      document.close();
-		      i++;
+				// Add some content
+				document.add(new Paragraph("Hello, World!"));
+
+				// Close the document
+				document.close();
+				i++;
 			}
-		    } catch (Exception e) {
-		      e.printStackTrace();
-		      
-		    
-		    }
-		 
-		 
-		 System.out.println("itext PDF program executed");
-		
-		  }
+		} catch (Exception e) {
+			e.printStackTrace();
+
 		}
 
+		System.out.println("itext PDF program executed");
+
+	}
+}
